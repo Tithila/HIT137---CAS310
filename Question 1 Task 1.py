3c23101aa65cc86a319ba3cea179a9d554ef5103
Tithila <s375319@students.cdu.edu.au>
2024-09-11 11:19:58 +0930

    Update Question 1 Task 1.py

--- Question 1 Task 1.py
+++ Question 1 Task 1.py
@@ -9,6 +9,6 @@ with open(output_file, 'w', encoding='utf-8') as txt_file:
             with open(filename, newline='', encoding='utf-8') as csvfile:
                 csvreader = csv.reader(csvfile)
                 for row in csvreader:
-                    txt_file.write(' '.join(row) + '\n'):
+                    txt_file.write(' '.join(row) + '\n')
 
 print(f"Text extracted from all CSV files in the current directory and saved to {output_file}")
